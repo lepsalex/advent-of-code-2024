b0ad58df35e844fe99a93c8cf5c87f8c8bcbe5ca
Alex Lepsa <lepsalex@gmail.com>
2024-12-03 18:06:48 -0500

    day-03 part-01

--- day-03/part-1/main.py
+++ day-03/part-1/main.py
@@ -1,14 +1,22 @@
-from lib.input import get_input_lines
+import re
+
+from lib.input import get_input
 
 
 def main():
-    lines = get_input_lines(__file__)
+    data = get_input(__file__)
 
-    for line in lines:
-        print(line)
+    # regex find all instances of mul(XXX,XXX) and put into capture groups
+    regex = re.compile("mul\\((\\d{1,3}),(\\d{1,3})\\)")
+    calls: list[tuple[str, str]] = regex.findall(data)
 
-    return True
+    # multiply result tuples
+    products = map(multiply, calls)
 
+    return sum(products)
+
+def multiply(data: tuple[str, str]):
+    return int(data[0]) * int(data[1])
 
 if __name__ == '__main__':
     result = main()
--- lib/input.py
+++ lib/input.py
@@ -1,8 +1,12 @@
 import os
 
 
-def get_input_lines(file: str):
+def get_input_lines(file: str) -> list[str]:
     with open(os.path.dirname(file) + '/../data/input') as file:
-        lines = [line.rstrip() for line in file]
+        lines = [line.strip() for line in file]
 
     return lines
+
+def get_input(file: str) -> str:
+    with open(os.path.dirname(file) + '/../data/input') as file:
+        return file.read()
