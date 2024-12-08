3aa962db038a598a360866105ca06016a7207008
Alex Lepsa <lepsalex@gmail.com>
2024-12-07 22:55:44 -0500

    day-07 part-02

--- day-07/solution/test_main.py
+++ day-07/solution/test_main.py
@@ -1,10 +1,11 @@
 from unittest import TestCase
 
 from lib.input import clean_input
-from main import solve_part_one
+from main import solve_part_one, solve_part_two
+
 
 class Test(TestCase):
-    def test_solve(self):
+    def test_solve_part_one(self):
         input_str = clean_input('''
         190: 10 19
         3267: 81 40 27
@@ -19,5 +20,23 @@ class Test(TestCase):
 
         expected = 3749
         actual = solve_part_one(input_str)
+
+        self.assertEqual(expected, actual)
+
+    def test_solve_part_two(self):
+        input_str = clean_input('''
+        190: 10 19
+        3267: 81 40 27
+        83: 17 5
+        156: 15 6
+        7290: 6 8 6 15
+        161011: 16 10 13
+        192: 17 8 14
+        21037: 9 7 18 13
+        292: 11 6 16 20
+        ''')
+
+        expected = 11387
+        actual = solve_part_two(input_str)
 
         self.assertEqual(expected, actual)
