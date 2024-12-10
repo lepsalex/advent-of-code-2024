de1ab386b36a60769a12304500709131ce9ba88f
Alex Lepsa <lepsalex@gmail.com>
2024-12-09 19:29:23 -0500

    day-09 part-02

--- day-09/solution/main.py
+++ day-09/solution/main.py
@@ -4,7 +4,7 @@ from lib.input import get_input
 def main():
     input_str = get_input(__file__)
 
-    return solve_part_one(input_str)
+    return solve_part_one(input_str), solve_part_two(input_str)
 
 
 def solve_part_one(input_str: str):
@@ -45,6 +45,79 @@ def solve_part_one(input_str: str):
 
         # increment the left cursor for the next pass
         lc += 1
+
+    return sum_total
+
+
+def solve_part_two(input_str: str):
+    current_id = 0
+    file_map = []
+
+    # construct the file map
+    for idx, char in enumerate(input_str):
+        num_repeats = int(char)
+
+        # use the index for file blocks, and the '.' char for free space
+        # note: we alternate between file blocks and free space (blocks are even)
+        write_char = current_id if idx % 2 == 0 else '.'
+
+        for _ in range(num_repeats):
+            file_map.append(write_char)
+
+        if idx % 2:
+            current_id += 1
+
+    # rearrange the file map
+    lc, rc = 0, len(file_map) - 1
+    sum_total = 0
+
+    # use left and right cursors to process the map
+    while lc <= rc:
+        # sum up moved blocks (update left cursor)
+        while file_map[lc] != '.':
+            lc += 1
+
+        # move right cursor to end of next block
+        while file_map[rc] == '.':
+            rc -= 1
+
+        # get the block we are trying to move
+        block_end = rc
+        block_start = block_end
+        block_char = file_map[block_end]
+        while file_map[block_start] == block_char:
+            block_start -= 1
+
+        block_size = block_end - block_start
+
+        # go through all possible free slots until one fits
+        free_block_start = lc
+        while free_block_start < block_start:
+            free_block_end = free_block_start
+            while file_map[free_block_end] == '.':
+                free_block_end += 1
+
+            free_block_size = free_block_end - free_block_start
+
+            if free_block_size >= block_size:
+                # copy the block to the free space
+                for copy_offset in range(block_size):
+                    file_map[free_block_start + copy_offset] = file_map[block_start + copy_offset + 1]
+                    file_map[block_start + copy_offset + 1] = '.'
+                # exit after copy
+                break
+            else:
+                # get the next free space block
+                free_block_start = free_block_end + 1
+                while file_map[free_block_start] != '.':
+                    free_block_start += 1
+
+        rc = block_start
+
+    # count up the final state of the file map
+    for idx, char in enumerate(file_map):
+        if char != '.':
+            sum_total = sum_total + (idx * int(char))
 
     return sum_total
 
--- day-09/solution/test_main.py
+++ day-09/solution/test_main.py
@@ -1,5 +1,5 @@
 from unittest import TestCase
-from main import solve_part_one
+from main import solve_part_one, solve_part_two
 
 class Test(TestCase):
     def test_solve_part_one(self):
@@ -7,5 +7,13 @@ class Test(TestCase):
 
         expected = 1928
         actual = solve_part_one(input_str)
+
+        self.assertEqual(expected, actual)
+
+    def test_solve_part_two(self):
+        input_str = "2333133121414131402"
+
+        expected = 2858
+        actual = solve_part_two(input_str)
 
         self.assertEqual(expected, actual)
