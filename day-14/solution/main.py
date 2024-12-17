4095d2d5727800817c03e4fceae674ae0da6f1d4
Alex Lepsa <lepsalex@gmail.com>
2024-12-16 23:39:34 -0500

    day-14 part-02

--- day-14/solution/main.py
+++ day-14/solution/main.py
@@ -9,7 +9,7 @@ from itertools import groupby, product
 def main():
     lines = get_input_lines(__file__)
 
-    return solve_part_one(lines, 101, 103), solve_part_two(lines)
+    return solve_part_one(lines, 101, 103), solve_part_two(lines, 101, 103)
 
 
 def solve_part_one(lines: list[str], w: int, h: int, steps=100):
@@ -20,6 +20,29 @@ def solve_part_one(lines: list[str], w: int, h: int, steps=100):
     quads_agg = {k: len(list(v)) for (k, v) in groupby(sorted(quads)) if k != 0}
 
     return reduce(mul, quads_agg.values(), 1)
+
+
+def solve_part_two(lines: list[str], w: int, h: int):
+    quad_func = to_quad_func(w, h)
+    pos_vel = [parse_line(line) for line in lines]
+
+    min_sf = float("inf")
+    best_iteration = None
+
+    for second in range(w * h):
+        results = []
+
+        for px, py, vx, vy in pos_vel:
+            results.append(((px + vx * second) % w, (py + vy * second) % h))
+
+        quads = map(quad_func, results)
+        quads_agg = {k: len(list(v)) for (k, v) in groupby(sorted(quads)) if k != 0}
+        sf = reduce(mul, quads_agg.values(), 1)
+        if sf < min_sf:
+            min_sf = sf
+            best_iteration = second
+
+    return best_iteration
 
 
 def parse_line(line: str) -> [int, int, int, int]:
@@ -55,10 +78,6 @@ def to_quad_func(w: int, h: int):
         return 0
 
     return to_quad
-
-
-def solve_part_two(lines: list[str]):
-    return 2
 
 
 if __name__ == '__main__':
--- day-14/solution/test_main.py
+++ day-14/solution/test_main.py
@@ -3,6 +3,7 @@ from unittest import TestCase
 from lib.input import clean_input
 from main import solve_part_one, solve_part_two
 
+
 class Test(TestCase):
     def test_solve_part_one(self):
         lines = clean_input('''
@@ -27,13 +28,21 @@ class Test(TestCase):
 
     def test_solve_part_two(self):
         lines = clean_input('''
-                0000
-                0000
-                0000
-                0000
+                p=0,4 v=3,-3
+                p=6,3 v=-1,-3
+                p=10,3 v=-1,2
+                p=2,0 v=2,-1
+                p=0,0 v=1,3
+                p=3,0 v=-2,-2
+                p=7,6 v=-1,-3
+                p=3,0 v=-1,-2
+                p=9,3 v=2,3
+                p=7,3 v=-1,2
+                p=2,4 v=2,-3
+                p=9,5 v=-3,-3
                 ''')
 
-        expected = 2
-        actual = solve_part_two(lines)
+        expected = 7
+        actual = solve_part_two(lines, 11, 7)
 
         self.assertEqual(expected, actual)
